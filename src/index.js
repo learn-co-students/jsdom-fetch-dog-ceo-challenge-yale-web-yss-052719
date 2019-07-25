4141f47f4775278614452cc93704ca52d72722d2
jqzheng617 <jenna.zheng617@gmail.com>
2019-07-24 22:33:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,53 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
- fetch("https://dog.ceo/api/breeds/image/random/4")
- .then(response => response.json())
- .then(data => {
-  // console.log(Object.keys(data))
-  // console.log(data.message)
-  // Prints result from `response.json()` in getRequest
-   data.message.forEach((url) => {
-     let img = document.createElement("img");
-     img.setAttribute("src", url);
-     document.getElementById("dog-image-container").appendChild(img)
-     // finding the parent (div)tag
-     //append ele to parent div
-   });
- })
 
- fetch('https://dog.ceo/api/breeds/list/all')
- .then(response => response.json())
- .then(data => {
-   // console.log(Object.keys(data.message))
-   let ul = document.getElementById("dog-breeds");
-   // locate the dog breeds and save it into an variable (ul)
-   let keys = Object.keys(data.message)
-   // get the object(data.message) of the breed names, set into a varaible (keys)
-   for (let i = 0; i < keys.length; i++) {
-     // iterate through the names to get a name, add name each time
-     let li = document.createElement("li");
-     // create the li element, set it in a variable
-     li.innerText = keys[i]
-     // set the keys(breed name) as text in <li>
-     document.body.append(li)
-     // print out the names at the end of the document
-   }
- })
+document.addEventListener('DOMContentLoaded', function(e){
+  // console.log('loaded')
+  // Challenge 1
+  fetch(imgUrl)
+  .then(res => res.json())
+  .then(data => {
+    // console.log(data)
+    data.message.forEach(function(e){
+      // returning all images
+      // console.log(e)
+      const imgTag = document.createElement('img')
+      imgTag.src = e
+      const dogImageContainer = document.getElementById("dog-image-container")
+      dogImageContainer.appendChild(imgTag)
+    })
+  })
 
- // challenge 3
- document.body.addEventListener('click', function(event) {
-   if(event.target.tagName === "LI") {
-     event.target.style.color = "red"
-   }
- })
-
- // challenge 4
- // document.querySelector('select').addEventListener('change', function(event){
- //   console.log(event.target.value)
- //   fetch('https://dog.ceo/api/breeds/list/all')
- //   .then(response => response.json())
- //   .then(data => {
- //     let filterBreed = Object.keys(data.message).filter(breed => breed.startWith(letter))
- // })
-
-});
+  // challenge 2
+  const dogBreeds = document.getElementById("dog-breeds")
+  fetch(breedUrl)
+  .then(res => res.json())
+  .then(data => {
+    // console.log(data)
+    for(let breeds in data.message){
+      // console.log(data.message[breeds])
+      const liTag = document.createElement('li')
+      liTag.innerText = breeds
+      dogBreeds.appendChild(liTag)
+    }
+  })
+  // challenge 3
+  dogBreeds.addEventListener('click', function(e){
+    // console.log(e)
+    if(e.target.tagName === 'LI'){
+      // console.log(e.target)
+      e.target.style.color ='purple'
+    }
+  })
+  // challenge 4
+  document.getElementById("breed-dropdown").addEventListener('change', function(e){
+    // console.log(e.target.value)
+    let letter = e.target.value
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then(data => {
+      // need to clear dog breeds
+      dogBreeds.innerHTML = ""
+      for(let breeds in data.message){
+        if(breeds.startsWith(letter)){
+          const liTag = document.createElement('li')
+          liTag.innerHTML = breeds
+          dogBreeds.appendChild(liTag)
+        }
+      }
+    })
+  })
+})
